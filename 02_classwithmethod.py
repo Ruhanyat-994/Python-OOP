596be548776020317c189e7c764740df372d3706
al-ruhanyat <alruhanyat994@gmail.com>
2022-09-20 18:02:32 +0600

    5th from here

--- 02_classwithmethod.py
+++ 02_classwithmethod.py
@@ -6,10 +6,15 @@ class Employee : # it's a blue print
 
     def printObj(self):
         print(f"The name is {self.name}")
-
+    @staticmethod 
+    def greet():
+        print("Good day")
 ruhanyat = Employee() # A basic object
 print(ruhanyat.marks)
 print(ruhanyat.center)
 print(ruhanyat.name) 
 
-ruhanyat.printObj()
+ruhanyat.printObj() # Employee.printObj(ruhanyat)
+Employee.printObj(ruhanyat)
+ruhanyat.greet()
+Employee.greet()
