d645aa9c3e0477fc62a17c03966a70459f7052b2
al-ruhanyat <alruhanyat994@gmail.com>
2022-09-20 17:46:23 +0600

    1st commit

---
+++ 01_class.py
@@ -0,0 +1,9 @@
+'''A very basic sample class'''
+class Employee : # it's a blue print
+    name = "Ruhanayt"
+    marks = 34
+    center = "Dhaka"
+ruhanyat = Employee() # A basic object
+print(ruhanyat.marks)
+print(ruhanyat.center)
+print(ruhanyat.name)
